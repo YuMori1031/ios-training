3b8c4b7518359ca975cbcfb68794d8079f151dc4
yu_m <koituha23@gmail.com>
2023-01-25 06:22:30 +0900

    Session 2. API

--- ios-training/ViewController.swift
+++ ios-training/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func reloadButton(_ sender: UIButton) {
+        do {
+            guard let result = try? YumemiWeather.fetchWeatherCondition(at: "tokyo") else { return }
+            
+            switch result {
+            case "sunny":
+                weatherImageView.image = UIImage(named: "Sunny")
+                weatherImageView.tintColor = .red
+            case "cloudy":
+                weatherImageView.image = UIImage(named: "Cloudy")
+                weatherImageView.tintColor = .gray
+            case "rainy":
+                weatherImageView.image = UIImage(named: "Rainy")
+                weatherImageView.tintColor = .blue
+            default:
+                return
+            }
+        }
+    }
+    
 }
 
