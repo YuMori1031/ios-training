fe779a973a3ff2a6a025a7da15893e7e8c0e6515
yu_m <koituha23@gmail.com>
2023-01-29 13:53:47 +0900

    Session 3.Lifecycle

--- ios-training/SceneDelegate.swift
+++ ios-training/SceneDelegate.swift
@@ -17,6 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // 起動画面をNewViewControllerに変更
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = NewViewController.init()
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
